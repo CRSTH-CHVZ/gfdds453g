9a4eb7d488e871a60d920cab23585671c5a71669
Cristhian-Chavez <crsth.1990@fpuna.edu.py>
2023-02-01 05:22:26 -0300

    Solve exercise

--- src/App.js
+++ src/App.js
@@ -9,12 +9,25 @@ import {
 export class App extends Component {
   render() {
     return (
-      <div>
-        <nav>
-          {/*Aca deben ir los links de navegacion*/}
-        </nav>
-        {/* Aca tienes que agreager algo para que las rutas funcionen*/}
-      </div>
+        <div>
+          <nav>
+            {/*Aca deben ir los links de navegacion*/}
+            <ul>
+              <li>
+                <Link to="/Page1">Pagina 1</Link>
+              </li>
+              <li>
+                <Link to="/Page2">Pagina 2</Link>
+              </li>
+            </ul>
+          </nav>
+          {/* Aca tienes que agreager algo para que las rutas funcionen*/}
+          <Switch>
+            <Route exact path={['/Page1', '/']} component={Page1} />
+            <Route exact path="/Page2" component={Page2} />
+            <Route component={NotFound} />
+          </Switch>
+        </div>
     )
   }
 }
@@ -25,18 +38,18 @@ export class App extends Component {
 
 export const Page1 = (props) => {
   return (
-    <h1>Pagina 1</h1>
+      <h1>Pagina 1</h1>
   )
 }
 
 export const Page2 = (props) => {
   return (
-    <h1>Pagina 2</h1>
+      <h1>Pagina 2</h1>
   )
 }
 
 export const NotFound = (props) => {
   return (
-    <h1>Pagina no encontrada</h1>
+      <h1>Pagina no encontrada</h1>
   )
 }
